5534cf4c6b921ef5e39e540c2fe54a5d31fdc46b
Andrew Chan <andrew.chan23@ycdsbk12.ca>
2021-01-18 15:59:19 +0000

    Monday Jan 18

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ rounds = 0
 points = 0
 
 print("---Welcome to the Jeopardy Game of ICS201a---")
-print("There are :200: :400: :600: :800: :1000: point options.\nYou will only get 10 questions to gain the most amount of points. \nPlease select the letter corresponding to your answer. \nGoal to beat is 10000! points")
+print("There are :200: :400: :600: :800: :1000: point options.\n\nYou will only get 10 questions to gain the most amount of points. \nPlease select the letter corresponding to your answer. \nGoal to beat is 10000 points!")
+print("\nEnter the number value (e.g 200, 400")
+
 
 
 while rounds <  10:
@@ -28,12 +30,13 @@ while rounds <  10:
       answer1 = answer1.lower()
 
       if answer1 == "a":
-        print("CORRECT")
+        print("\nCORRECT")
         points += 200
         print("Score: ",points)
       else:
-        print("INCORRECT")
+        print("\nINCORRECT")
         print("Score: ",points)
+
     elif question_200 == 2:
       print("What does GPU stand for? ")
       print("A.) Global Production Unit \nB.) Graphics Processer \nC.) Graphics Processing Unit")
@@ -41,11 +44,11 @@ while rounds <  10:
       answer2 = answer2.lower()
 
       if answer2 == "c":
-        print("CORRECT")
+        print("\nCORRECT")
         points += 200
         print("Score: ",points)
       else:
-        print("INCORRECT")
+        print("\nINCORRECT")
         print("Score: ",points)
 
     elif question_200 == 3:
@@ -56,9 +59,56 @@ while rounds <  10:
       answer3 = answer3.lower()
 
       if answer3 == "c":
-        print("CORRECT")
+        print("\nCORRECT")
         points += 200
         print("Score: ",points)
       else:
-        print("INCORRECT")
+        print("\nINCORRECT")
         print("Score: ",points)
+
+  elif option == 400:
+    print("You picked a 400 point question.")
+
+    if question_400 == 1:
+      print("What does Ransomware do? ")
+
+      print("A.) Disguises itself as legitimate software \nB.) Threatens to publish the victim's data or  block access to it  \nC.) Aims to gather information about a person or organization")
+      answer4 = input("Answer: ")
+      answer4 = answer4.lower()
+
+      if answer4 == "b":
+        print("\nCORRECT")
+        points += 400
+        print("Score: ",points)
+      else:
+        print("\nINCORRECT")
+        print("Score: ",points)
+
+    elif question_400 == 2:
+      print("What is most common type of Malware ")
+
+      print("A.) Adware \nB.) Trojan Horse  \nC.) Spyware \nD.")
+      answer5 = input("Answer: ")
+      answer5 = answer5.lower()
+
+      if answer4 == "a":
+        print("\nCORRECT")
+        points += 400
+        print("Score: ",points)
+      else:
+        print("\nINCORRECT")
+        print("Score: ",points)
+
+
+  else:
+    print("Invalid option")
+    rounds += 1
+
+
+if points >= 10000:
+  print("You beat the record!")
+
+else:
+  print("You did not beat the record of 10000 :(")
+
+
